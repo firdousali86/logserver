116cd47da818afd13ee6491dcbd8715efc837379
Firdous Ali <fali@digital-dividend.com>
2020-04-16 16:42:25 +0500

    create date on logs

--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Project) {
+    Project.beforeRemote('prototype.__create__logs', function(ctx, modelInstance, next) {
 
+        if(ctx && ctx.args && ctx.args.data){
+          ctx.args.data.createdAt = Date.now();
+        }
+        
+        next();
+      });
+
+    // Project.beforeRemote('**', function(ctx, user, next) {
+    //   console.log(ctx.methodString, 'was invoked remotely'); // customers.prototype.save was invoked remotely
+    //   next();
+    // });
 };
